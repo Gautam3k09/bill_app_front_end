ca2041ece56bae02df4f0f21f0d5363db7473686
Gautam jha <gautamgjhaj@gmail.com>
2023-06-13 19:33:27 +0530

    commit

--- src/app/Home-tab/company-bill-info-modal/company-bill-info-modal.component.ts
+++ src/app/Home-tab/company-bill-info-modal/company-bill-info-modal.component.ts
@@ -16,8 +16,12 @@ import html2canvas from 'html2canvas';
   styleUrls: ['./company-bill-info-modal.component.css']
 })
 export class CompanyBillInfoModalComponent {
-   pdf :any=''
-   slicedDate : any = ''
+  totalAmountWithoutTax:any=0
+  sgstAmount:any=0
+  cgstAmount:any=0
+  igstAmount:any=0
+  pdf :any=''
+  slicedDate : any = ''
   @Input() data: any;
   constructor(private modalService: NgbModal) { }
 
@@ -34,16 +38,27 @@ var imgWidth = 208;
 var pageHeight = 295;
 var imgHeight = canvas.height * imgWidth / canvas.width;
 var heightLeft = imgHeight;
- 
 const contentDataURL = canvas.toDataURL('image/png')
 let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF
 var position = 0;
-pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, 300)
 pdf.save(this.data.companyName + '_' + this.slicedDate  + '.pdf'); // Generated PDF
 });
 }
   ngOnInit() {
-    // console.log(this.data.address);
+    console.log(this.data);
+    
+    for (let i = 0; i < this.data.item.length; i++) {
+      this.totalAmountWithoutTax = parseFloat(this.totalAmountWithoutTax) + parseFloat(this.data.item[i].amount) ;
+      if(this.data.item[i].igst){
+        this.igstAmount = parseFloat(this.igstAmount) + parseFloat(this.data.item[i].igstamount) ;
+      }
+      if(this.data.item[i].cgst){
+        this.cgstAmount = parseFloat(this.cgstAmount) + parseFloat(this.data.item[i].cgstamount) ;
+        this.sgstAmount = parseFloat(this.sgstAmount) + parseFloat(this.data.item[i].sgstamount) ;
+      }
+    }
+    console.log(this.totalAmountWithoutTax ,this.sgstAmount,this.cgstAmount,this.igstAmount ,'amountwihtou')
     /* Output:
      {prop1: "Some Data", prop2: "From Parent Component", prop3: "This Can be anything"}
     */
